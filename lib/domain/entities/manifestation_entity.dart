de8dd1ea90a5d901ea3af0a5c898a4c454514289
vini.rod <vinicioswork@gmail.com>
2024-06-10 14:42:23 -0300

    manifestation entity

--- lib/domain/entities/manifestation_entity.dart
+++ lib/domain/entities/manifestation_entity.dart
@@ -1,1 +1,35 @@
-class ManifestationEntity {}
+import 'package:equatable/equatable.dart';
+import 'package:melhoracidade/domain/entities/citizen_entity.dart';
+
+enum ManifestationStatus {
+  paused,
+  inProgress,
+  fixedUp,
+}
+
+class ManifestationEntity extends Equatable {
+  final String idManifestation;
+  final String idAuthor;
+  final String title;
+  final String description;
+  final String images;
+  final ManifestationStatus status;
+  final List<CitizenEntity> supportters;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  ManifestationEntity(
+      {required this.idManifestation,
+      required this.idAuthor,
+      required this.title,
+      required this.description,
+      required this.images,
+      required this.status,
+      required this.supportters,
+      required this.createdAt,
+      required this.updatedAt});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [idManifestation];
+}
